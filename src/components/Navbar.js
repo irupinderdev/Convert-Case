937b434e4a195296cc60d15382c938796b2e5ebc
rupinder <inforupinder79@gmail.com>
2022-04-10 22:27:30 +0530

    search btn color changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,7 @@ export default function Navbar(props) {
         </ul>
         <form className="d-flex">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-success" type="submit">Search</button>
+          <button className="btn btn-warning" type="submit">Search</button>
         </form>
       </div>
     </div>
