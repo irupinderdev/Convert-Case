c305364ee91dc93342e2bbba2c51f032713f96f3
Rupinder Singh <inforupinder79@gmail.com>
2022-09-06 20:18:24 +0530

    navbar removed, background changed

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,11 +5,11 @@ import { NavLink } from 'react-router-dom'
 export const Navbar = (props) => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light">
+      <nav className="text-center navbar navbar-expand-lg navbar-light">
         {/* <nav ClassName= {`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}> */}
-        <div className="container-fluid">
-          <NavLink className="navbar-brand font-weight-bold" to="/">{props.title}</NavLink>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <div className="container">
+          <NavLink className="m-auto navbar-brand font-weight-bold" to="/"><b>{props.title}</b></NavLink>
+          {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -36,7 +36,7 @@ export const Navbar = (props) => {
 
             </ul>
 
-          </div>
+          </div> */}
         </div>
       </nav>
     </>
